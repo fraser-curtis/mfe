e37d597cb1178bb131203a2a430379ca811b68f3
Curtis Fraser <13140709+fraser-curtis@users.noreply.github.com>
2022-10-24 14:22:55 -0600

    add stupid commit

---
+++ container/src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { MarketingApp } from './components/marketing-app';
+
+
+export const App = () => {
+    return (<div>
+        <h1>Hi there</h1>
+        <hr/>
+        <MarketingApp/>
+        </div>)
+}
