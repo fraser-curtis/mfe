fa23747ca3d5e91060f47aa1ad7a5a1389874e84
Curtis Fraser <13140709+fraser-curtis@users.noreply.github.com>
2022-10-25 16:45:48 -0600

    adding header to the website and fixing css issue

--- container/src/app.js
+++ container/src/app.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Header } from './components/header';
 import { MarketingApp } from './components/marketing-app';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'container'
+});
 
 export const App = () => {
-    return (<div>
-        <h1>Hi there again and again!!! </h1>
-        <hr/>
-        <MarketingApp/>
-        </div>)
+    return (
+        <BrowserRouter>
+            <StylesProvider generateClassName={generateClassName}>
+                <div>
+                    <Header/>
+                    <MarketingApp/>
+                </div>
+            </StylesProvider>
+        </BrowserRouter>
+    )
 }
