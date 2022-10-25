aa7b0927d8e171ae5cb55142b10755e7d57a34bd
Curtis Fraser <13140709+fraser-curtis@users.noreply.github.com>
2022-10-25 17:28:06 -0600

    Using memory history instead of browser history

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,19 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './app';
+import {App} from './app';
+import { createMemoryHistory} from 'history'
 
-const mount = (el) => {
+const mount = (el, { onNavigate }) => {
+    const history = createMemoryHistory();
+
+    if(onNavigate) {
+        history.listen(onNavigate);
+    }
+
+
     ReactDOM.render(
-        <App />        ,
+        <App history={history} />        ,
         el
     )
+
+    return {
+        onParentNavigate({pathname: nextPathname}){
+            if(history.location.pathname !== nextPathname) {
+                history.push(nextPathname);
+            }
+        }
+    };
 };
+
+
 
 if(process.env.NODE_ENV === 'development') {
     const el = document.querySelector("#marketing-dev-root");
 
     if(el) {
-        mount(el);
+        mount(el, {});
     }
 }
 
